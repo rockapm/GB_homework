4f013120681023e6c1601fa585e9a86c45ef74e0
Alexey <habarov1796@gmail.com>
2022-08-11 16:06:57 +0300

    TASK 2 MIN MAX NUMBER created

---
+++ homework_1/task2_MinMaxNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Simple program to find maximum of two numbers.");
+Console.Write("Enter 'a': ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 'b': ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (a < b) { max = b; }
+
+Console.WriteL("Max number: ");
+Console.WriteLine(max);
+Console.Write("---end---");
