53709fcd9c29e66a405a0600f5c0e13762a4be61
Alexey <habarov1796@gmail.com>
2022-08-11 16:21:47 +0300

    TASK 4 created, TASK 2 modified

---
+++ homework_1/task4_MaxFrom3Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Simple program to find maximum of three numbers.");
+Console.Write("Enter 'a': ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 'b': ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 'c': ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (max < b) { max = b; }
+if (max < c) { max = c; }
+
+Console.Write("Max number: ");
+Console.WriteLine(max);
+Console.Write("---end---");
