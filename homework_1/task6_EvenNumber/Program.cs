ba512a5dd40aef48080dec499a551da2e96fd86d
Alexey <habarov1796@gmail.com>
2022-08-11 16:42:10 +0300

    TASK 6 created

---
+++ homework_1/task6_EvenNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Simple program to determine the parity of a number.");
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0) { Console.WriteLine("Even (true)"); }
+else { Console.WriteLine("Uneven (false)"); }
+
+Console.Write("---end---");
