8dd01c4c177b6418ad6489d000cfbc11f034f145
Alexey <habarov1796@gmail.com>
2022-08-11 16:59:42 +0300

    TASK 8 created

---
+++ homework_1/task8_FindAllEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Simple program to find all parity numbers.");
+Console.WriteLine("Enter number: ");
+int a = 1;
+int b = Convert.ToInt32(Console.ReadLine());
+
+for (int i = a; i <= b; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+}
+
+
+Console.Write("; end");
