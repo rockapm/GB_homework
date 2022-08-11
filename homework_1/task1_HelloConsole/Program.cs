26d6e959f1daf8f3d7fbd0e9d8b606322096a5a7
Alexey <habarov1796@gmail.com>
2022-08-11 15:53:43 +0300

    TASK 1 SIMPLE PROGRAM created

---
+++ homework_1/task1_HelloConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("This is simple program for example.");
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(name);
+//end
