9b5ec49f25201bf98d8b4eda80b404df32dd8ee7
Alexey <habarov1796@gmail.com>
2022-08-18 19:26:57 +0300

    Created Example003, 004

---
+++ work_1/example003/Program.cs
@@ -0,0 +1,17 @@
+﻿//Находим в массиве индекс равный find и завершаем программу
+int[] array = { 11, 12, 13, 4, 18, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
