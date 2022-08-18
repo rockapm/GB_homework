95047d2b422944336c564d06c6d35c2a85bc2956
Alexey <habarov1796@gmail.com>
2022-08-18 19:49:58 +0300

    Created Task19

---
+++ homework_2/task19_palindrom/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите четырехзначное число");
+int number = int.Parse(Console.ReadLine());
+int pal = 0, num = number;
+while (number > 0)
+{
+    pal = pal * 10 + number % 10; number /= 10;
+}
+Console.WriteLine(num == pal ? "Палиндром" : "Не палиндром");
