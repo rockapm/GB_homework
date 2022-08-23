ac843d385e23c869bed5279bcf3566372b69a8bb
Alexey <habarov1796@gmail.com>
2022-08-24 01:37:50 +0300

    Add Multiplication Table to mytrain

---
+++ mytrain/ex3_MultiplicationTable/Program.cs
@@ -0,0 +1,15 @@
+﻿//ТАБЛИЦА УМНОЖЕНИЯ
+
+void MultTable()
+{
+    for (int i = 2; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            Console.WriteLine($"{i} * {j} = {i * j}");
+        }
+        Console.WriteLine();
+    }
+}
+
+MultTable();
