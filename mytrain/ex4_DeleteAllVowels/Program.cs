634f28aa887f9b3784a6ca94ed2bef85c75595d7
Alexey <habarov1796@gmail.com>
2022-08-25 01:38:58 +0300

    Add ex4 in folder Mytrain

---
+++ mytrain/ex4_DeleteAllVowels/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача: удалить все гласные из вводимого текста
+using System.Text.RegularExpressions;
+
+string Disemvowel(string str)
+{
+    string str1 = str;
+    string str2 = Regex.Replace(str1, "[AaEeIiOoUu]", "");
+
+    return str2;
+}
+
+Console.WriteLine("Enter text:");
+string str = Console.ReadLine();
+Console.WriteLine(Disemvowel(str));
+
